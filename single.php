22b72c0456d5ae99294d84a6c0649698498c1183
jonsorre <hello@jonsorrentino.com>
2017-12-04 22:52:01 -0500

    Figured out how to create unique gradients per post/page. Need to make sure I add in a Background Color field in ACF plugin. The labels are currently set to bgcolor1 and bgcolor2.

--- single.php
+++ single.php
@@ -9,7 +9,7 @@
  * @since 		Starkers 4.0
  */
 ?>
-<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header' ) ); ?>
+<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header-proj' ) ); ?>
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
